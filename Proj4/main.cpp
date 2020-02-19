3768faa5e61e85083329d24b41b613f43bc77e64
Liwei Lu <ricardoluliwei@gmail.com>
2020-02-18 19:17:01 -0800

    Initial Commit

---
+++ Proj4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Proj4
+//
+//  Created by Ricardo on 2/18/20.
+//  Copyright © 2020 Ricardo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+   return 0;
+}
