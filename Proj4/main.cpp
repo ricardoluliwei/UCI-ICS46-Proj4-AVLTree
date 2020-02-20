e91fc59b5ac6a3cd9d9831306f0bee9757230ccb
Liwei Lu <ricardoluliwei@gmail.com>
2020-02-19 18:07:44 -0800

    finished find

--- Proj4/main.cpp
+++ Proj4/main.cpp
@@ -1,10 +1,22 @@
 #include <fstream>
 #include <iostream>
+#include "MyAVLTree.hpp"
+using namespace std;
 
+void testFind(){
+    MyAVLTree<int, string> tree;
+    tree.insert(5, "foo");
+    tree.insert(3, "sna");
+    tree.insert(10, "bar");
+    tree.insert(12, "twelve");
+    cout<< tree.find(12) << endl;
+    tree.find(12) = "It's me now";
+    cout << tree.find(12) << endl;
+}
 
 int main()
 {
-
+    testFind();
     return 0;
 }
 
